210c8b60c7ec32e0e1ea3e3191e313bb42d2144c
urahimono <urahimono@gmail.com>
2024-01-28 00:55:29 +0900

    [UI] Emotion のお金UIを修正しました

--- EgaoKoubouUnity/Assets/UI/Scripts/MoneyUI.cs
+++ EgaoKoubouUnity/Assets/UI/Scripts/MoneyUI.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class MoneyUI : MonoBehaviour
 {
@@ -9,13 +12,20 @@ public class MoneyUI : MonoBehaviour
     [SerializeField]
     public int AmountMoney;
 
-    public List<MoneyImageUI> moneyList = new List<MoneyImageUI>();
+
+
+    private List<MoneyImageUI> moneyList = new List<MoneyImageUI>();
 
 
     // Start is called before the first frame update
     void Start()
     {
-        InstantiateMoney();
+        // InstantiateMoney();
+        foreach (Transform ts in transform)
+        {
+            moneyList.Add(ts.GetComponent<MoneyImageUI>());
+        }
+        moneyList = moneyList.OrderByDescending(value => value.name).ToList();
     }
 
     // Update is called once per frame
@@ -24,15 +34,17 @@ public class MoneyUI : MonoBehaviour
 
     }
 
-    public void InstantiateMoney()
-    {
-        for (int i = 0; i < AmountMoney; i++)
-        {
-            var money = Instantiate(moneyImagesUI, transform);
-            moneyList.Add(money);
-        }
-
-    }
+//     private void InstantiateMoney()
+//     {
+//         for (int i = 0; i < AmountMoney; i++)
+//         {
+//             var money = Instantiate(moneyImagesUI, transform);
+//             money.name = $"[{i:D2}]{money.name}";
+//             moneyList.Add(money);
+//         }
+//         moneyList = moneyList.OrderByDescending(value => value.name).ToList();
+// 
+//     }
 
     public void UseMoney(int money)
     {
@@ -43,5 +55,23 @@ public class MoneyUI : MonoBehaviour
             moneyList.Remove(obj);
         }
     }
+
+#if UNITY_EDITOR
+
+    [ContextMenu("Create Item")]
+    private void CreateItem()
+    {
+        moneyList.Clear();
+        for (int i = 0; i < AmountMoney; ++i)
+        {
+            var item = PrefabUtility.InstantiatePrefab(moneyImagesUI, transform) as MoneyImageUI;
+            item.name = $"[{i:D2}]{item.name}";
+            // moneyList.Add(item);
+        }
+
+        // moneyList.Sort((a, b) => Mathf.CeilToInt(a.myAnchoredPostion.y - b.myAnchoredPostion.y));
+    }
+
+#endif
 
 }
