1a87d4d07cf47231d9fb6fa152d3162ea7aa53f9
Seibe TAKAHASHI <t2seibe.jp>
2024-01-28 09:23:48 +0900

    feat: タイトル画面を追加

---
+++ EgaoKoubouUnity/Assets/Scripts/TitlePresenter.cs
@@ -0,0 +1,20 @@
+#nullable enable
+using VContainer.Unity;
+
+public class TitlePresenter : IStartable, ITickable, System.IDisposable
+{
+    void IStartable.Start()
+    {
+        // todo
+    }
+
+    void ITickable.Tick()
+    {
+        // todo
+    }
+
+    void System.IDisposable.Dispose()
+    {
+        // todo
+    }
+}
